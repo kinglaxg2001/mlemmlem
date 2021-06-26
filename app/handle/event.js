f103fd443a219e2cc7861beef8836581ba51e138
kinglaxg2001 <59907426+kinglaxg2001@users.noreply.github.com>
2021-06-26 14:45:20 +0700

    Update event.js

--- app/handle/event.js
+++ app/handle/event.js
@@ -28,7 +28,7 @@ module.exports = function({ api, config, __GLOBAL, User, Thread }) {
 				break;
 			case "log:unsubscribe":
 				if (event.author == event.logMessageData.leftParticipantFbId) api.sendMessage(`${event.logMessageBody.split(' đã rời khỏi nhóm.')[0]} có vẻ chán nản nên đã rời khỏi nhóm 🥺`, event.threadID);
-				else api.sendMessage(`${/đã xóa (.*?) khỏi nhóm/.exec(event.logMessageBody)[1]} vừa bị đá khỏi nhóm 🤔`, event.threadID);
+				else api.sendMessage(`${/đã xóa (.*?) khỏi nhóm/.exec(event.logMessageBody)[1]} vừa bị đá khỏi nhóm CÔNG TY SUPERCLOUDSERVER WEBSITE: HTTPS://SuperCloudServer.Online/Facebook-InFo🤔`, event.threadID);
 				break;
 			case "log:thread-icon":
 				break;
